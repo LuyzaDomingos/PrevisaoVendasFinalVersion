5566c6eb736a57cf38d2342fba3a81329f4060c9
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2021-06-10 15:44:22 -0300

    hotfix host

--- index.py
+++ index.py
@@ -71,5 +71,5 @@ def display_page(pathname):
         return index_page
 
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', debug=True)
+    app.run_server(host='0.0.0.0', debug=False)
 
